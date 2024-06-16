a577dabf3ae48638707bfdb4dc4c14f0b81932f6
bo98117 <bosin98117@gmail.com>
2024-06-16 08:26:38 -0700

    isValid

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -32,14 +32,20 @@ const isValid = s => {
     for (let i = 0; i < s.length; i++) {
         let char = s[i];
 
-        //
+        // If pairsHashMap[char] is a key, then we push it into our stack[].
+        // stack.push(char) = key
         if (pairsHashMap[char]) {
             stack.push(char);
+            // Now we have pushed the keys: (, {, and [ into our stack[].
+            // So now if we pop the values out of our stack[] and it's not equal to the char, then we would return false.
+            // stack.pop() = value
         } else if (pairsHashMap[stack.pop()] !== char) {
             return false;
         }
     }
 
+    // We do not return true; because the stack may contain keys that were not popped off.
+    // We return stack.length === 0; because we want to make sure that there are no other keys left in our stack.  This statement would be equal to returning true.
     return stack.length === 0;
 };
 
@@ -49,3 +55,4 @@ module.exports = isValid;
 Time Complexity: O(n) - We iterate through the input string just once
 Space Complexity: O(n) - In worst case, stack is same length as input string
 */
+//
