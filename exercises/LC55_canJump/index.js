7cc98c2d6f213fa2cd47610de57c30d9c4e48fb3
bo98117 <bosin98117@gmail.com>
2024-07-06 08:11:11 -0700

    index

--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,7 +1,7 @@
 /*
 You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
-Return true if you can reach the last index, or false otherwise. 
+Return true if you can reach the last index, or false otherwise.
 
 Example 1:
 Input: nums = [2,3,1,1,4]
@@ -11,7 +11,7 @@ Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 Example 2:
 Input: nums = [3,2,1,0,4]
 Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index. 
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 Constraints:
 1 <= nums.length <= 104
@@ -19,23 +19,23 @@ Constraints:
 */
 
 // Start filling out if true or false from the end of array
-function canJump(nums) {
-    const memo = [];
-    memo[nums.length - 1] = true;
-    // Start filling out if true or false from the end of array
-    for (let i = nums.length - 2; i >= 0; i--) {
-        const maxIndex = Math.min(nums.length - 1, i + nums[i]);
-        memo[i] = false;
-        for (let j = i + 1; j <= maxIndex; j++) {
-            if (memo[j]) {
-                memo[i] = true;
-            }
-        }
-    }
-    return memo[0];
-}
+// function canJump(nums) {
+//     const memo = [];
+//     memo[nums.length - 1] = true;
+//     // Start filling out if true or false from the end of array
+//     for (let i = nums.length - 2; i >= 0; i--) {
+//         const maxIndex = Math.min(nums.length - 1, i + nums[i]);
+//         memo[i] = false;
+//         for (let j = i + 1; j <= maxIndex; j++) {
+//             if (memo[j]) {
+//                 memo[i] = true;
+//             }
+//         }
+//     }
+//     return memo[0];
+// }
 
-module.exports = canJump;
+// module.exports = canJump;
 
 /*
 Time Complexity:
@@ -107,7 +107,7 @@ function canJump(nums, index = 0) {
 }
 */
 
-/*
+
 // Dynamic Programming
 function canJump(nums) {
     // Create an array with the same length as the input array and fill all indices with FALSE
@@ -126,4 +126,3 @@ function canJump(nums) {
     }
     return dpPositions[dpPositions.length - 1];
 }
-*/
