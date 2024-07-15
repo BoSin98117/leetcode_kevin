926fc36f34f2e31c94752d93bc15d0df02e009a6
bo98117 <bosin98117@gmail.com>
2024-07-15 06:55:40 -0700

    maximum profit

--- exercises/LC121_maxProfit/index.js
+++ exercises/LC121_maxProfit/index.js
@@ -26,7 +26,7 @@ var maxProfit = function (prices) {
     let maxProfit = 0;
     let cheapestPrice = prices[0];
 
-    for (let i = 0; i < prices.length; i++) {
+    for (let i = 1; i < prices.length; i++) {
         const price = prices[i];
 
         console.log("price = " + prices[i]);
